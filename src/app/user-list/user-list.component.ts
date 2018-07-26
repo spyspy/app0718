52b7554af67f9759ff1b0dbce6395c5610859ae2
spyspy <spyhopping101@gmail.com>
2018-07-26 22:55:06 +0800

    rxjs update

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
-// import { Observable, throwError} from 'rxjs';
-// import { retry, catchError,} from 'rxjs/operators';
-
+import { throwError, of,from, Observable } from 'rxjs';
+import { retry, catchError , map , filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-list',
@@ -22,6 +21,8 @@ export class UserListComponent implements OnInit {
     // 為什麼這邊可以直接抓到 userlist
     // this.userlist.ngOnInit().subscribe();
     // 必須有return值才可以訂閱( subscribe() )
+   
+
     this.userlist.getUrl().subscribe(
       userlist => {
         return this.users = userlist;
@@ -33,6 +34,32 @@ export class UserListComponent implements OnInit {
     // this.userlist.getUrl().subscribe(
     //   (abc:UserService) => this.users = UserService
     // );
+
+    //How to use RxJS
+
+    //Print out: One by One
+    let data = [1,2,3];
+    from(data).subscribe(test=>console.log(test));
+
+    //Print out: The whole array
+    let data2 = [333, 555, 777];
+    of(data2).subscribe(num=>console.log(num));
+    
+    //Formal Expression
+    let data3 = ['koko', 'kiki', 'boyo'];
+    of(data3).subscribe({
+      next: function(value) {
+          console.log(value)
+      },
+      complete: function() {
+          console.log('complete!');
+      },
+      error: function(error) {
+          console.log(error)
+      }
+  });
+
+
   }
 
 }
